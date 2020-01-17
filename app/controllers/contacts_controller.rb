afcc4d4e03c2f3a96f4dbbef5a48c12cbb462c78
Freddy Silber <freddy.silber@cloudcoach.com>
2020-01-17 15:43:52 -0700

    stubbing out contacts controller

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,2 +1,25 @@
 class ContactsController < ApplicationController
+	def new
+		@contact = Contact.new
+	end
+
+	def create 
+		contact = Contact.new(contact_params)
+		if contact.valid?
+			contact.save
+			redirect_to '/contacts/show'
+		else
+			redirect_to '/'
+		end
+	end
+
+	def show
+		@contacts = Contact.all
+	end
+
+	private
+
+	def user_params
+    	params.require(:contact).permit(:first_name, :last_name, :phone_number, :email)
+  	end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,19 +8,20 @@ class UsersController < ApplicationController
     if user.valid?
       user.save
       session[:user_id] = user[:id]
-      redirect_to "/users/#{user.id}"
+      redirect_to '/users/#{user.id}'
     else
-      redirect_to "/"
+      redirect_to '/'
     end
   end
 
   def show
-    if logged_in?
+    if logged_in? # is this even needed????
       @user = User.find(params[:id])
     end
   end
 
   private 
+
   def user_params
     params.require(:user).permit(:username, :password)
   end
