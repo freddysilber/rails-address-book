2032ea5153470c5da9f3669b6b400a5426d80ba8
Freddy Silber <freddy.silber@cloudcoach.com>
2020-01-17 21:27:17 -0700

    removed contacts controller comments

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -9,37 +9,15 @@ class ContactsController < ApplicationController
 
 	def create 
 		contact = Contact.new(contact_params)
-		# contact.user_id = current_user.id
-		
 		if contact.valid?
 			contact.save
 			redirect_to "/contacts/#{contact.id}"
 		else
 			raise
-			# redirect_to '/'
 		end
-		# raise params
-		# @contact = Contact.new
-		# @contact.first_name = params[:first_name]
-		# @contact.save
-		# redirect_to contact_path(@contact)
-
-		# raise contact_params
-		# raise params
-		# contact = Contact.create(contact_params)
-		# raise contact.valid?
-		# redirect_to "/contacts/#{contact.id}"
-		# raise contact_params
-		# contact = Contact.new(contact_params)
-		# # raise contact.valid?
-		# if contact.valid?
-		# 	contact.save
-		# 	redirect_to "/contacts/#{contact.id}"
-		# end
 	end
 
 	def show
-		# @contact = Contact.find_by(id: params[:id])
 		@contact = Contact.find(params[:id])
 		render 'show'
 	end
