7189eb63243e17bfa619600a4342f772d0b7265c
Freddy Silber <freddy.silber@cloudcoach.com>
2020-01-17 22:49:43 -0700

    indentation = 4

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,28 +4,28 @@ class UsersController < ApplicationController
 	end
 
 	def create
-	user = User.new(user_params)
-	if user.valid?
-		user.save
-		session[:user_id] = user[:id]
-		redirect_to "/users/#{user.id}"
-	else
-		redirect_to '/'
-	end
+		user = User.new(user_params)
+		if user.valid?
+			user.save
+			session[:user_id] = user[:id]
+			redirect_to "/users/#{user.id}"
+		else
+			redirect_to '/'
+		end
 	end
 
 	def show
-	@user = User.find(params[:id])
-	if current_user == @user
-		render 'show'
-	else 
-		redirect_to '/'
-	end
+		@user = User.find(params[:id])
+		if current_user == @user
+			render 'show'
+		else 
+			redirect_to '/'
+		end
 	end
 
 	private 
 
 	def user_params
-	params.require(:user).permit(:username, :password)
+		params.require(:user).permit(:username, :password)
 	end
 end
