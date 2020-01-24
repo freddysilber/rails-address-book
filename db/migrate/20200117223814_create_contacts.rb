c0354188caab320874dfb8b61e9e507450b254c5
Freddy Silber <freddy.silber@cloudcoach.com>
2020-01-24 10:05:51 -0700

    order migrations to alphabetical order
    establish relationships with foriegn keys

--- db/migrate/20200117223814_create_contacts.rb
+++ db/migrate/20200117223814_create_contacts.rb
@@ -1,11 +1,12 @@
 class CreateContacts < ActiveRecord::Migration[5.2]
   def change
     create_table :contacts do |t|
+      t.string :email
       t.string :first_name
       t.string :last_name
       t.integer :phone_number
-      t.string :email
-      t.integer :user_id
+
+      t.integer :account_id
 
       t.timestamps
     end
--- db/migrate/20200124034634_create_accounts.rb
+++ db/migrate/20200124034634_create_accounts.rb
@@ -1,12 +1,12 @@
 class CreateAccounts < ActiveRecord::Migration[5.2]
   def change
     create_table :accounts do |t|
-      t.string :name
-      t.integer :contact_id
-      t.integer :user_id
-      t.integer :phone_number
-      t.boolean :active
       t.string :account_type
+      t.boolean :active
+      t.string :name
+      t.integer :phone_number
+
+      t.integer :user_id
 
       t.timestamps
     end
