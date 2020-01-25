8f8083baa41ee6bc8f4a28df98363609d23bd985
Freddy Silber <freddy.silber@cloudcoach.com>
2020-01-25 12:13:01 -0700

    refactored _account to show checkbox/ routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,14 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'users#index'
   resources :users
   resources :contacts
   resources :sessions
-  # resources :accounts
   resources :accounts do
     resources :contacts
   end
 
   get '/signin' => 'sessions#new'
+	get '/logout' => 'sessions#destroy'
   post '/signin' => 'sessions#create'
 	post '/logout' => 'sessions#destroy'
-	
-	get '/logout' => 'sessions#destroy'
 end
