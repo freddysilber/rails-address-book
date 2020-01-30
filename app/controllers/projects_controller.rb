1cdd8888901a4816f001aeb4a6f2089bf0906a90
Freddy Silber <freddy.silber@cloudcoach.com>
2020-01-30 11:05:19 -0700

    project actions/ project views

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,10 +1,63 @@
 class ProjectsController < ApplicationController
 	def index
-		@projects = Project.all
+		@projects = []
+		Project.all.each do |p|
+			if p.account.user_id == current_user.id
+				@projects << p
+			end
+		end
 		@project = self.new
 	end	
 
 	def new
 		@project = Project.new
+	end
+
+
+	def create
+		project = Project.new(project_params)
+		project.project_name = project_params[:project_name].capitalize
+		if project.valid?
+			project.save
+			redirect_to '/projects'
+		else
+			raise
+		end
+	end
+
+	def show
+		@project = Project.find(params[:id])
+		if @project.account.user_id == current_user.id
+			render 'show'
+		end
+	end
+
+	def edit
+		@project = Project.find(params[:id])
+		if @project.account.user_id == current_user.id
+			render 'edit'
+		end
+	end
+
+	def update
+		@project = Project.find(params[:id])
+		@project.update(project_params)
+		redirect_to project_path(@project)
+	end
+
+	def destroy
+		Project.find(params[:id]).destroy
+		redirect_to '/projects'
+	end
+
+	private
+
+	def project_params
+		params.require(:project).permit(
+			:project_name,
+			:account_id,
+			:owner,
+			:complete
+		)
 	end
 end
