3a93d1d7bebc5094c7da4d0bdc592f0b6b7fc975
Freddy Silber <freddy.silber@cloudcoach.com>
2020-02-01 15:46:38 -0700

    updating comments

--- app/models/account.rb
+++ app/models/account.rb
@@ -2,5 +2,5 @@ class Account < ApplicationRecord
 	belongs_to :user
 	has_many :contacts
 	has_many :projects
-	# has_many :tasks, through: :projects
+	has_many :tasks, through: :projects
 end
--- app/models/task.rb
+++ app/models/task.rb
@@ -1,13 +1,3 @@
 class Task < ApplicationRecord
 	belongs_to :project
-
-	# TASK_STATUS = [
-	# 	'Open',
-	# 	'In Progress',
-	# 	'Complete'
-	# ]
-
-	# def self.task_statuses
-	# 	TASK_STATUS
-	# end
 end
