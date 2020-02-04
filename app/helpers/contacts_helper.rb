11b2341f96b5cc282aa0e467e6705cabd47cd2be
Freddy Silber <freddy.silber@cloudcoach.com>
2020-02-03 20:30:20 -0700

    enforcing ownership

--- app/helpers/contacts_helper.rb
+++ app/helpers/contacts_helper.rb
@@ -1,7 +1,7 @@
 module ContactsHelper
 	def account_id_field(contact)
 		if contact.account.nil?
-			select_tag "contact[account_id]", options_from_collection_for_select(Account.all, :id, :name)
+			select_tag "contact[account_id]", options_from_collection_for_select(Account.all.where(user_id: current_user.id), :id, :name), class: "field"
 		else
 			hidden_field_tag "contact[account_id]", contact.account_id
 		end
