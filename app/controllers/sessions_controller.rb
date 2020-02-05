9dc9f95541b56ee458cb6f19cf45ac53a33e5143
Freddy Silber <freddy.silber@cloudcoach.com>
2020-02-04 17:06:32 -0700

    refactoring  o-auth

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,20 +2,23 @@ require 'securerandom'
 class SessionsController < ApplicationController
 	def create
 		if auth_hash = request.env["omniauth.auth"]
-			oauth_username = request.env["omniauth.auth"]["info"]["nickname"]
-			if user = User.find_by(:username => oauth_username)
-				session[:user_id] = user.id
-				redirect_to "/users/#{user.id}"
-			else
-				user_password = SecureRandom.hex
-				@user = User.new(:username => oauth_username, :password => user_password)
-				if @user.save
-					session[:user_id] = @user.id
-					redirect_to "/users/#{@user.id}"
-				else
-					render 'sessions/new'
-				end
-			end
+			user = User.find_or_create_by_omniauth(auth_hash)
+			session[:user_id] = user.id
+			redirect_to "/users/#{user.id}"
+			# oauth_username = request.env["omniauth.auth"]["info"]["nickname"]
+			# if user = User.find_by(:username => oauth_username)
+			# 	session[:user_id] = user.id
+			# 	redirect_to "/users/#{user.id}"
+			# else
+			# 	user_password = SecureRandom.hex
+			# 	@user = User.new(:username => oauth_username, :password => user_password)
+			# 	if @user.save
+			# 		session[:user_id] = @user.id
+			# 		redirect_to "/users/#{@user.id}"
+			# 	else
+			# 		render 'sessions/new'
+			# 	end
+			# end
 		else
 			user = User.find_by(:username => params[:username])
 			if user && user.authenticate(params[:password])
--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,26 @@ class User < ApplicationRecord
 	has_many :accounts, :dependent => :destroy
 	has_many :contacts, through: :accounts, :dependent => :destroy
 	has_many :projects, through: :accounts, :dependent => :destroy
+	# 3RD PARTY LOGIN (OMNIAUTH W/ GITHUB)
+	def self.find_or_create_by_omniauth(auth_hash)
+		oauth_username = auth_hash["info"]["nickname"]
+		if user = User.find_by(:username => oauth_username)
+			# session[:user_id] = user.id
+			# redirect_to "/users/#{user.id}"
+			return user
+		else
+			# user_password = SecureRandom.hex
+			user = User.create(:username => oauth_username, :password => SecureRandom.hex)
+			# return user
+			# if user.save
+			# 	return user
+			# 	# session[:user_id] = user.id
+			# 	# redirect_to "/users/#{user.id}"
+			# else
+			# 	render 'sessions/new'
+			# end
+		end
+	end
 	# GET ALL ACCOUNTS BY CURRENT USER
 	def self.my_accounts(current_user_id)
 		accounts = []
